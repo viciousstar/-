f13f0c5be0e083dcb4bd2fef909bbca97c461e9d
viciousstar <vicious0star@gmail.com>
2015-10-17 20:09:08 +0800

    create user model

---
+++ HIT_Groups/Users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from Groups.models import Group
+# Create your models here.
+class User(models.Model):
+    """
+    Description: User Description
+    """
+    name = models.CharField()
+    email = models.EmailField()
+    # password
+    # headimage
+
+    groups = models.ManyToManyField(Group, through = "UsersAndGroups")
+
+class UsersAndGroups(models.Model):
+    """
+    Description: For many to many relationship and User Authority Description
+    """
+
+    user_id = models.ForeignKey(User) 
+    group_id = models.ForeignKey(Group)
+    user_role = models.CharField(choices = 
+                                (('Creator', 'Creator'),
+                                    ("Admin", "Admin"),
+                                    ("User", "User")), default="User")
+
