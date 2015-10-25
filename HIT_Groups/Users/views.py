ef7fa1da1813d1afbd4eb2a5560f27650baff34b
viciousstar <vicious0star@gmail.com>
2015-10-25 10:35:32 +0800

    give up modify rest-frame UI

--- HIT_Groups/Users/views.py
+++ HIT_Groups/Users/views.py
@@ -14,18 +14,18 @@ class UserList(generics.ListCreateAPIView):
     """docstring for UserList"""
     queryset = MyUser.objects.all()
     serializer_class = UserSerializer
-    renderer_classes = (TemplateHTMLRenderer, )
-    template_name = "Users/base.html"
+    # renderer_classes = (TemplateHTMLRenderer, )
+    # template_name = "Users/base.html"
 
+    # # def list(self, request):
+    #     # return render(request, self.template_name, {"objects": queryset})
     # def list(self, request):
-        # return render(request, self.template_name, {"objects": queryset})
-    def list(self, request):
-        # Note the use of `get_queryset()` instead of `self.queryset`
-        queryset = self.get_queryset()
-        serializer = UserSerializer(queryset, many=True)
-        return render(request, self.template_name, {"objects": serializer.data})
+    #     # Note the use of `get_queryset()` instead of `self.queryset`
+    #     queryset = self.get_queryset()
+    #     serializer = UserSerializer(queryset, many=True)
+    #     return render(request, self.template_name, {"objects": serializer.data})
 
-        # return Response(serializer.data)
+    #     # return Response(serializer.data)
 
 
 class UserDetail(generics.RetrieveUpdateAPIView):
