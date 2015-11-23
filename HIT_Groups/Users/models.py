482b26d9804566daea37f91bd3aac75fb7d5b7ad
viciousstar <vicious0star@gmail.com>
2015-11-23 23:03:39 +0800

    add some authority control func

--- HIT_Groups/Users/models.py
+++ HIT_Groups/Users/models.py
@@ -3,18 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from Groups.models import Group
 
 # Create your models here.
-class MyUser(AbstractUser):
-    """
-    Description: User Description
-
-    Todo:
-        description
-        tag
-    """
-    my_groups = models.ManyToManyField(Group, through = "UsersAndGroups")
-    # password
-    # headimage
-
 class UsersAndGroups(models.Model):
     """
     Description: For many to many relationship and User Authority Description
@@ -29,3 +17,36 @@ class UsersAndGroups(models.Model):
                                  (('Creator', 'Creator'),
                                   ("Admin", "Admin"),
                                   ("User", "User")), default="User", max_length = 20)
+
+class MyUser(AbstractUser):
+    """
+    Description: User Description
+
+    Todo:
+        description
+        tag
+    """
+    my_groups = models.ManyToManyField(Group, through = "UsersAndGroups")
+    # password
+    # headimage
+    def has_role_creator(self, group):
+        return True if \
+            UsersAndGroups.objects.filter(user_id = self, group_id = group, user_role="Creator") \
+            else False
+
+    def has_role_admin(self, group):
+        return True if \
+            UsersAndGroups.objects.filter(user_id = self, group_id = group, user_role="Admin") \
+            else False
+
+    def has_role_user(self, group):
+        return True if \
+            UsersAndGroups.objects.filter(user_id = self, group_id = group, user_role="User") \
+            else False
+
+    def can_modify(self, obj):
+        if obj.creator() == self:
+            return True
+        else:
+            return False
+
